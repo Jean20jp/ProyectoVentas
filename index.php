816c711a020fc9cacaeddbd173e4b7148afa3b15
Juan <zapjuanfra97@gmail.com>
2021-08-08 01:05:55 -0500

    importar clases en el index

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 require_once "controlador/plantilla.controlador.php";
 require_once "controlador/administrador.controlador.php";
+require_once "controladores/productos.controlador.php";
+require_once "controladores/categoria.controlador.php";
+
+require_once "modelos/administrador.modelo.php";
+require_once "modelos/productos.modelo.php";
+require_once "modelos/categoria.modelo.php";
+require_once "modelos/rutas.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
