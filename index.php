c27236346053d788f8d2c6c1e50f1e8aa8878135
Fernando <gatoguevara2@hotmail.com>
2021-08-07 19:16:14 -0500

    modificacion index

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once "controlador/plantilla.controlador.php";
 require_once "controlador/administrador.controlador.php";
 
-require_once "modelo/administrador.modelo.php";
-require_once "modelo/rutas.php";
+//require_once "modelo/administrador.modelo.php";
+//require_once "modelo/rutas.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
